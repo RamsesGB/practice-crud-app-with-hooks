b67bc4705f8f2a862077334bdb050008980ca6a5
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-09-01 23:51:09 -0400

    init react app, created dummy data, created user table component

--- hooks-practice-app/src/App.js
+++ hooks-practice-app/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserTable  from './tables/UserTable';
 
 function App() {
+  const usersData = [
+    {id: 1, name:'Ramses', userName:'RanchiGB'},
+    {id: 2, name:'Carlos', userName:'Carlitosrojas'},
+    {id: 3, name:'Andrea', userName:'Dreacreazz'},
+  ]
+
+  const [users, setUsers] = useState(usersData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>CRUD App with Hooks</h1>
+      <div className='flex-row'>
+        <div className='flex-large'>
+          <h2>Add user</h2>
+        </div>
+        <div className='flex-large'>
+          <h2>View users</h2>
+          <UserTable users={users}/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ hooks-practice-app/src/tables/UserTable.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const UserTable = props => (
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.users.length > 0 ? (
+        props.users.map(user => (
+          <tr>
+            <td>Name data</td>
+            <td>Username data</td>
+            <td>
+              <button className="button muted-button">Edit</button>
+              <button className="button muted-button">Delete</button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No users</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+);
+
+export default UserTable;
