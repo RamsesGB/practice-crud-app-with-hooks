a6a2e7b0a4c93fab401554bab08393c88f5a241f
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-09-02 01:20:50 -0400

    create add user form component and passed adduser function down

--- hooks-practice-app/src/App.js
+++ hooks-practice-app/src/App.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import UserTable  from './tables/UserTable';
+import AddUserForm from './forms/AddUserForm'
 
 function App() {
   const usersData = [
-    {id: 1, name:'Ramses', userName:'RanchiGB'},
-    {id: 2, name:'Carlos', userName:'Carlitosrojas'},
-    {id: 3, name:'Andrea', userName:'Dreacreazz'},
+    {id: 1, name:'Ramses', username:'RanchiGB'},
+    {id: 2, name:'Carlos', username:'Carlitosrojas'},
+    {id: 3, name:'Andrea', username:'Dreacreazz'},
   ]
 
   const [users, setUsers] = useState(usersData);
+
+  const addUser = user => {
+    user.id = users.length + 1
+    setUsers([...users, user])
+  }
 
   return (
     <div className='container'>
@@ -16,6 +22,7 @@ function App() {
       <div className='flex-row'>
         <div className='flex-large'>
           <h2>Add user</h2>
+          <AddUserForm addUser={addUser}/>
         </div>
         <div className='flex-large'>
           <h2>View users</h2>
